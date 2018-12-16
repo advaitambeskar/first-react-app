033e2e84f28d54658fd6b520cdc978f7ec5e55d2
Advait Ambeskar <ambeskaradvait@gmail.com>
2018-12-16 15:54:46 -0500

    Changes to About.jsx

--- app/about.jsx
+++ app/about.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 
-class About{
+class About extends {
     render(){
-
+        const deviceStatus = DetectMobile.detectmob;
+        var name;
+        if(deviceStatus === true) {
+            this.props.description = 'a.';
+        }
+        else{
+            this.props.description = 'I AM ADVAIT AMBESKAR.';
+        }
         return(
-            <div className = "firstName">
-                <p className = "position">{this.props.name}</p>
+            <div className = "aboutHeader">
+                <div className = "aboutRow">
+                    <div>
+                        
+                    </div>
+                    <div>
+                        <h1 className = "aboutTitle"><u>About Me</u></h1>
+                        <p className = "aboutPosition">{this.props.description}</p>
+                        (essentially description can be different for mobile and the desktop user)
+
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
             </div>
         )
     }
